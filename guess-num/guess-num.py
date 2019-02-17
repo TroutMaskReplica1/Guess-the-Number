bd9fac8ef9c9bcd46d1aab6b9faedd950a9addd7
Ryan Dikeos <ryan.dikeos@gmail.com>
2019-02-17 12:08:12 -0800

    add guess game

--- guess-num/guess-num.py
+++ guess-num/guess-num.py
@@ -1,1 +1,17 @@
-print('I\'m for test purposes!')
+import random
+
+i = 1
+
+guess = int(input('Guess a number '))
+x = random.randint(1, 10)
+while i < 11:
+    if guess == x:
+        print('Ya won! Only took ya {i} tries!')
+        break
+    elif i == 10:
+        print("Too Slow Joe!")
+        break
+    else:
+        guess2 = int(input('Guess again '))
+        y = random.randint(1, 10)
+        i += 1
